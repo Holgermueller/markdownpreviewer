708e316ac3e44e44b798d73c9162634dc2f8d94b
Holger Mueller <9jholger@gmail.com>
2022-01-08 21:43:59 -0600

    basic scaffold of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const marked = require("marked");
 
 function App() {
+  const [mrkdwn] = useState(
+    `
+    #Heading
+
+    ##Subheading
+    `
+  );
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Markdown Previewer</h1>
       </header>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="form-floating">Editor </div>
+
+            <textarea
+              type="text"
+              id="editor"
+              className="form-control-lg"
+              value={mrkdwn}
+            ></textarea>
+          </div>
+
+          <div className="col">
+            <div className="preview-label">Preview</div>
+            <div id="preview" className="preview-area"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
